7397cf6a578289d0c4f8ede5ca457a240d487f48
giuliobosco <giuliobva@gmail.com>
2019-08-19 15:43:07 +0200

    add db.sql - create db

---
+++ db.sql
@@ -0,0 +1,140 @@
+CREATE DATABASE dbe;
+
+CREATE TABLE dbe.audit_action
+(
+    id           INT AUTO_INCREMENT,
+
+    created_by   INT         NOT NULL,
+    created_date DATETIME    NOT NULL,
+    updated_by   INT         NOT NULL,
+    updated_date DATETIME    NOT NULL,
+    deleted_by   INT,
+    deleted_date DATETIME,
+
+    name         VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dbe.audit_action_audit
+(
+    id           INT AUTO_INCREMENT,
+    audit_id     INT         NOT NULL,
+    action       INT         NOT NULL,
+
+    created_by   INT         NOT NULL,
+    created_date DATETIME    NOT NULL,
+    updated_by   INT         NOT NULL,
+    updated_date DATETIME    NOT NULL,
+    deleted_by   INT,
+    deleted_date DATETIME,
+
+    audit_name   VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (audit_id) REFERENCES dbe.audit_action (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (action) REFERENCES dbe.audit_action (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE TABLE dbe.users
+(
+    id           INT AUTO_INCREMENT,
+
+    created_by   INT                NOT NULL,
+    created_date DATETIME           NOT NULL,
+    updated_by   INT                NOT NULL,
+    updated_date DATETIME           NOT NULL,
+    deleted_by   INT,
+    deleted_date DATETIME,
+
+    username     VARCHAR(32) UNIQUE NOT NULL,
+    password     CHAR(64)           NOT NULL,
+    salt         CHAR(32)           NOT NULL,
+    firstname    VARCHAR(64)        NOT NULL,
+    lastname     VARCHAR(64)        NOT NULL,
+    email 	 VARCHAR(128) UNIQUE NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (created_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (updated_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (deleted_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE TABLE dbe.users_audit
+(
+    id              INT AUTO_INCREMENT,
+    audit_id        INT                NOT NULL,
+    action          INT                NOT NULL,
+
+    created_by      INT                NOT NULL,
+    created_date    DATETIME           NOT NULL,
+    updated_by      INT                NOT NULL,
+    updated_date    DATETIME           NOT NULL,
+    deleted_by      INT,
+    deleted_date    DATETIME,
+
+    audit_username  VARCHAR(32) UNIQUE NOT NULL,
+    audit_password  CHAR(64)           NOT NULL,
+    audit_salt      CHAR(32)           NOT NULL,
+    audit_firstname VARCHAR(64)        NOT NULL,
+    audit_lastname  VARCHAR(64)        NOT NULL,
+    audit_email     VARCHAR(128)       NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (audit_id) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (action) REFERENCES dbe.audit_action (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (created_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (updated_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (deleted_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE TABLE dbe.record (
+    id INT AUTO_INCREMENT,
+    
+    created_by INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    updated_by INT NOT NULL,
+    updated_date DATETIME NOT NULL,
+    deleted_by INT,
+    deleted_date DATETIME,
+
+    firstname VARCHAR (64) NOT NULL,
+    lastname VARCHAR(64) NOT NULL,
+    born_date DATE NOT NULL,
+    address VARCHAR(128) NOT NULL,
+    email VARCHAR (128) NOT NULL,
+    phone VARCHAR (30) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (created_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (updated_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (deleted_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
+CREATE TABLE dbe.record_audit (
+    id INT AUTO_INCREMENT,
+    audit_id INT NOT NULL,
+    action INT NOT NULL,
+
+    created_by INT NOT NULL,
+    created_date DATETIME NOT NULL,
+    updated_by INT NOT NULL,
+    updated_date DATETIME NOT NULL,
+    deleted_by INT NOT NULL,
+    deleted_date INT NOT NULL,
+
+    firstname VARCHAR (64) NOT NULL,
+    lastname VARCHAR(64) NOT NULL,
+    born_date DATE NOT NULL,
+    address VARCHAR(128) NOT NULL,
+    email VARCHAR (128) NOT NULL,
+    phone VARCHAR (30) NOT NULL,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (audit_id) REFERENCES dbe.record (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (action) REFERENCES dbe.audit_action (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (created_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (updated_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE,
+    FOREIGN KEY (deleted_by) REFERENCES dbe.users (id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
+
